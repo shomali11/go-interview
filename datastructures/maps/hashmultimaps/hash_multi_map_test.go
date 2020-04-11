51646cf000ce9a515ed802cf40bf87b7b9a8cb32
Raed Shomali <shomali11@gmail.com>
2020-04-11 15:18:44 -0400

    Clean up

--- datastructures/maps/hashmultimaps/hash_multi_map_test.go
+++ datastructures/maps/hashmultimaps/hash_multi_map_test.go
@@ -7,43 +7,65 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHashMultiMap(t *testing.T) {
+func TestHashMultiMap_GetValues(t *testing.T) {
+	multiMap := New()
+	multiMap.Put("hello", 1)
+
+	assert.True(t, reflect.DeepEqual(multiMap.GetValues("hello"), []interface{}{1}))
+	assert.True(t, reflect.DeepEqual(multiMap.GetValues("unknown"), []interface{}{}))
+}
+
+func TestHashMultiMap_Contains(t *testing.T) {
 	multiMap := New()
 	multiMap.Put("hello", 1)
 
 	assert.Equal(t, multiMap.Contains("hello"), true)
-	assert.True(t, reflect.DeepEqual(multiMap.GetValues("hello"), []interface{}{1}))
 	assert.Equal(t, multiMap.Contains("unknown"), false)
-	assert.True(t, reflect.DeepEqual(multiMap.GetValues("unknown"), []interface{}{}))
 	assert.Equal(t, multiMap.ContainsAll("hello", "unknown"), false)
 	assert.Equal(t, multiMap.ContainsAny("hello", "unknown"), true)
-	assert.Equal(t, multiMap.Size(), 1)
-	assert.Equal(t, multiMap.GetKeys()[0], "hello")
+}
 
-	multiMap.Clear()
+func TestHashMultiMap_Clear(t *testing.T) {
+	multiMap := New()
+	assert.Equal(t, multiMap.IsEmpty(), true)
 	assert.Equal(t, multiMap.Size(), 0)
 
+	multiMap.Put("hello", 1)
+	assert.Equal(t, multiMap.IsEmpty(), false)
+	assert.Equal(t, multiMap.Size(), 1)
+
+	multiMap.Clear()
+	assert.Equal(t, multiMap.IsEmpty(), true)
+	assert.Equal(t, multiMap.Size(), 0)
+}
+
+func TestHashMultiMap_PutAll(t *testing.T) {
+	multiMap := New()
+
 	multiMap.PutAll(111, "x", "x", "y", "z")
-	assert.Equal(t, multiMap.Contains(111), true)
-	assert.Equal(t, multiMap.ContainsAll(111), true)
-	assert.Equal(t, multiMap.ContainsAny(111), true)
 	assert.True(t, reflect.DeepEqual(multiMap.GetValues(111), []interface{}{"x", "x", "y", "z"}))
 	assert.Equal(t, multiMap.Size(), 1)
 	assert.Equal(t, multiMap.GetKeys()[0], 111)
+}
 
+func TestHashMultiMap_Remove(t *testing.T) {
+	multiMap := New()
+
+	multiMap.PutAll(111, "x", "x", "y", "z")
 	multiMap.Remove(111, "x")
-	assert.Equal(t, multiMap.Contains(111), true)
-	assert.Equal(t, multiMap.ContainsAll(111), true)
-	assert.Equal(t, multiMap.ContainsAny(111), true)
+
 	assert.True(t, reflect.DeepEqual(multiMap.GetValues(111), []interface{}{"x", "y", "z"}))
 	assert.Equal(t, multiMap.Size(), 1)
 	assert.Equal(t, multiMap.GetKeys()[0], 111)
+}
 
+func TestHashMultiMap_RemoveKey(t *testing.T) {
+	multiMap := New()
+
+	multiMap.PutAll(111, "x", "x", "y", "z")
 	multiMap.RemoveKey(111)
+
 	assert.Equal(t, multiMap.Size(), 0)
-	assert.Equal(t, multiMap.Contains(111), false)
-	assert.Equal(t, multiMap.ContainsAll(111), false)
-	assert.Equal(t, multiMap.ContainsAny(111), false)
 	assert.True(t, reflect.DeepEqual(multiMap.GetValues(111), []interface{}{}))
 }
 
