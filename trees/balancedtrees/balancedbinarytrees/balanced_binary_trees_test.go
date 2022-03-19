df437a7e4496676688070a2d67d45e4272a97c25
Moein Nemati <moeinroid@gmail.com>
2022-03-19 15:40:37 +0330

    Rewrite balanced trees with generics

--- trees/balancedtrees/balancedbinarytrees/balanced_binary_trees_test.go
+++ trees/balancedtrees/balancedbinarytrees/balanced_binary_trees_test.go
@@ -8,34 +8,34 @@ import (
 )
 
 func TestIsBalanced_EmptyNode(t *testing.T) {
-	assert.True(t, IsBalanced(nil))
+	assert.True(t, IsBalanced[int](nil))
 }
 
 func TestIsBalanced_RootNode(t *testing.T) {
-	node := &trees.BinaryNode{Data: 1}
+	node := &trees.BinaryNode[int]{Data: 1}
 
 	assert.True(t, IsBalanced(node))
 }
 
 func TestIsBalanced_HalfTree(t *testing.T) {
-	node2 := &trees.BinaryNode{Data: 2}
-	node := &trees.BinaryNode{Data: 1, Left: node2}
+	node2 := &trees.BinaryNode[int]{Data: 2}
+	node := &trees.BinaryNode[int]{Data: 1, Left: node2}
 
 	assert.True(t, IsBalanced(node))
 }
 
 func TestIsBalanced_FullTree(t *testing.T) {
-	node2 := &trees.BinaryNode{Data: 2}
-	node3 := &trees.BinaryNode{Data: 3}
-	node := &trees.BinaryNode{Data: 1, Left: node2, Right: node3}
+	node2 := &trees.BinaryNode[int]{Data: 2}
+	node3 := &trees.BinaryNode[int]{Data: 3}
+	node := &trees.BinaryNode[int]{Data: 1, Left: node2, Right: node3}
 
 	assert.True(t, IsBalanced(node))
 }
 
 func TestIsBalanced_NotBalancedTree(t *testing.T) {
-	node2 := &trees.BinaryNode{Data: 2}
-	node3 := &trees.BinaryNode{Data: 3, Left: node2}
-	node := &trees.BinaryNode{Data: 1, Right: node3}
+	node2 := &trees.BinaryNode[int]{Data: 2}
+	node3 := &trees.BinaryNode[int]{Data: 3, Left: node2}
+	node := &trees.BinaryNode[int]{Data: 1, Right: node3}
 
 	assert.False(t, IsBalanced(node))
 }
--- trees/balancedtrees/balancedmultitrees/balanced_multi_trees_test.go
+++ trees/balancedtrees/balancedmultitrees/balanced_multi_trees_test.go
@@ -8,36 +8,36 @@ import (
 )
 
 func TestIsBalanced_EmptyNode(t *testing.T) {
-	assert.True(t, IsBalanced(nil))
+	assert.True(t, IsBalanced[int](nil))
 }
 
 func TestIsBalanced_RootNode(t *testing.T) {
-	node := &trees.MultiNode{Data: 1}
+	node := &trees.MultiNode[int]{Data: 1}
 
 	assert.True(t, IsBalanced(node))
 }
 
 func TestIsBalanced_HalfTree(t *testing.T) {
-	node2 := &trees.MultiNode{Data: 2}
-	node := &trees.MultiNode{Data: 1, Children: []*trees.MultiNode{node2}}
+	node2 := &trees.MultiNode[int]{Data: 2}
+	node := &trees.MultiNode[int]{Data: 1, Children: []*trees.MultiNode[int]{node2}}
 
 	assert.True(t, IsBalanced(node))
 }
 
 func TestIsBalanced_FullTree(t *testing.T) {
-	node2 := &trees.MultiNode{Data: 2}
-	node3 := &trees.MultiNode{Data: 3}
-	node := &trees.MultiNode{Data: 1, Children: []*trees.MultiNode{node2, node3}}
+	node2 := &trees.MultiNode[int]{Data: 2}
+	node3 := &trees.MultiNode[int]{Data: 3}
+	node := &trees.MultiNode[int]{Data: 1, Children: []*trees.MultiNode[int]{node2, node3}}
 
 	assert.True(t, IsBalanced(node))
 }
 
 func TestIsBalanced_NotBalancedTree(t *testing.T) {
-	node2 := &trees.MultiNode{Data: 2}
-	node3 := &trees.MultiNode{Data: 3, Children: []*trees.MultiNode{node2}}
-	node4 := &trees.MultiNode{Data: 4, Children: []*trees.MultiNode{node3}}
-	node5 := &trees.MultiNode{Data: 5}
-	node := &trees.MultiNode{Data: 1, Children: []*trees.MultiNode{node4, node5}}
+	node2 := &trees.MultiNode[int]{Data: 2}
+	node3 := &trees.MultiNode[int]{Data: 3, Children: []*trees.MultiNode[int]{node2}}
+	node4 := &trees.MultiNode[int]{Data: 4, Children: []*trees.MultiNode[int]{node3}}
+	node5 := &trees.MultiNode[int]{Data: 5}
+	node := &trees.MultiNode[int]{Data: 1, Children: []*trees.MultiNode[int]{node4, node5}}
 
 	assert.False(t, IsBalanced(node))
 }
