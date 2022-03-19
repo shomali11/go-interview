6c81264d03990747603387a9943efc8e1bf1049e
Moein Nemati <moeinroid@gmail.com>
2022-03-19 16:24:35 +0330

    Rewrite reverse with generics

--- lists/detectintersections/detect_intersections.go
+++ lists/detectintersections/detect_intersections.go
@@ -3,7 +3,7 @@ package detectintersections
 import "github.com/shomali11/go-interview/datastructures/linkedlists/singlylinkedlists"
 
 // DoIntersect checks if the two lists intersect
-func DoIntersect(head1 *singlylinkedlists.SLLNode, head2 *singlylinkedlists.SLLNode) bool {
+func DoIntersect[T comparable](head1 *singlylinkedlists.SLLNode[T], head2 *singlylinkedlists.SLLNode[T]) bool {
 	head1ListCount := getCountNodes(head1)
 	head2ListCount := getCountNodes(head2)
 
@@ -20,7 +20,7 @@ func DoIntersect(head1 *singlylinkedlists.SLLNode, head2 *singlylinkedlists.SLLN
 	return false
 }
 
-func getStartingNode(head *singlylinkedlists.SLLNode, moves int) *singlylinkedlists.SLLNode {
+func getStartingNode[T comparable](head *singlylinkedlists.SLLNode[T], moves int) *singlylinkedlists.SLLNode[T] {
 	current := head
 	for moves > 0 {
 		current = current.Next
@@ -29,7 +29,7 @@ func getStartingNode(head *singlylinkedlists.SLLNode, moves int) *singlylinkedli
 	return current
 }
 
-func getCountNodes(head *singlylinkedlists.SLLNode) int {
+func getCountNodes[T comparable](head *singlylinkedlists.SLLNode[T]) int {
 	count := 0
 	current := head
 	for current != nil {
--- lists/detectintersections/detect_intersections_test.go
+++ lists/detectintersections/detect_intersections_test.go
@@ -8,25 +8,25 @@ import (
 )
 
 func TestDoIntersect(t *testing.T) {
-	emptyList1 := singlylinkedlists.New()
-	emptyList2 := singlylinkedlists.New()
+	emptyList1 := singlylinkedlists.New[string]()
+	emptyList2 := singlylinkedlists.New[string]()
 	assert.False(t, DoIntersect(emptyList1.GetHead(), emptyList2.GetHead()))
 
 	nonEmptyList1 := singlylinkedlists.New("A", "B", "C")
 	nonEmptyList2 := singlylinkedlists.New("A", "B", "C")
 	assert.False(t, DoIntersect(nonEmptyList1.GetHead(), nonEmptyList2.GetHead()))
 
-	node1 := &singlylinkedlists.SLLNode{Value: "X"}
-	node2 := &singlylinkedlists.SLLNode{Value: "X"}
-	node3 := &singlylinkedlists.SLLNode{Value: "X"}
-	node4 := &singlylinkedlists.SLLNode{Value: "X"}
+	node1 := &singlylinkedlists.SLLNode[string]{Value: "X"}
+	node2 := &singlylinkedlists.SLLNode[string]{Value: "X"}
+	node3 := &singlylinkedlists.SLLNode[string]{Value: "X"}
+	node4 := &singlylinkedlists.SLLNode[string]{Value: "X"}
 
 	node1.Next = node2
 	node2.Next = node3
 	node3.Next = node4
 
-	node5 := &singlylinkedlists.SLLNode{Value: "X"}
-	node6 := &singlylinkedlists.SLLNode{Value: "X"}
+	node5 := &singlylinkedlists.SLLNode[string]{Value: "X"}
+	node6 := &singlylinkedlists.SLLNode[string]{Value: "X"}
 
 	node5.Next = node6
 	node6.Next = node4
