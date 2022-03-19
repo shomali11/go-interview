b5e6c422ced7b8b839bae685c7cb89ae5d25dd6a
Moein Nemati <moeinroid@gmail.com>
2022-03-19 16:34:39 +0330

    Rewrite running medians with generics

--- streams/runningmedians/running_medians.go
+++ streams/runningmedians/running_medians.go
@@ -4,20 +4,20 @@ import "github.com/shomali11/go-interview/datastructures/priorityqueues"
 
 // New generates a Running Median
 func New() *RunningMedian {
-	maxCompare := func(i, j interface{}) bool {
-		return i.(int) > j.(int)
+	maxCompare := func(i, j int) bool {
+		return i > j
 	}
 
-	minCompare := func(i, j interface{}) bool {
-		return i.(int) < j.(int)
+	minCompare := func(i, j int) bool {
+		return i < j
 	}
-	return &RunningMedian{minPQ: priorityqueues.New(minCompare), maxPQ: priorityqueues.New(maxCompare)}
+	return &RunningMedian{minPQ: priorityqueues.New[int](minCompare), maxPQ: priorityqueues.New[int](maxCompare)}
 }
 
 // RunningMedian keeps track of the running median
 type RunningMedian struct {
-	minPQ *priorityqueues.PriorityQueue
-	maxPQ *priorityqueues.PriorityQueue
+	minPQ *priorityqueues.PriorityQueue[int]
+	maxPQ *priorityqueues.PriorityQueue[int]
 }
 
 // Add adds a number to the running median calculations
@@ -38,12 +38,12 @@ func (rm *RunningMedian) GetMedian() float64 {
 	return (peek(rm.minPQ) + peek(rm.maxPQ)) / 2.0
 }
 
-func pop(pq *priorityqueues.PriorityQueue) int {
+func pop(pq *priorityqueues.PriorityQueue[int]) int {
 	value, _ := pq.Pop()
-	return value.(int)
+	return value
 }
 
-func peek(pq *priorityqueues.PriorityQueue) float64 {
+func peek(pq *priorityqueues.PriorityQueue[int]) float64 {
 	value, _ := pq.Peek()
-	return float64(value.(int))
+	return float64(value)
 }
