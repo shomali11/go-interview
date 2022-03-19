8e92ca41cf879ba798845185df315506141f4662
Moein Nemati <moeinroid@gmail.com>
2022-03-19 15:00:43 +0330

    Rewrite hash set with generics

--- datastructures/sets/hashsets/hash_set_test.go
+++ datastructures/sets/hashsets/hash_set_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestHashSet(t *testing.T) {
-	set := New()
+	set := New[string]()
 	assert.Equal(t, set.IsEmpty(), true)
 
 	set.Add("hello")
@@ -38,12 +38,12 @@ func TestHashSet(t *testing.T) {
 }
 
 func TestHashSetMerge(t *testing.T) {
-	set1 := New()
+	set1 := New[string]()
 	set1.Add("hello", "cool")
 
-	set2 := New("hello", "sweet")
+	set2 := New[string]("hello", "sweet")
 
-	set3 := New()
+	set3 := New[string]()
 	set3.Merge(set1, set2)
 
 	assert.Equal(t, set3.Size(), 3)
@@ -51,7 +51,7 @@ func TestHashSetMerge(t *testing.T) {
 }
 
 func TestHashSetCopy(t *testing.T) {
-	set1 := New()
+	set1 := New[string]()
 	set1.Add("a", "b", "c")
 	set2 := set1.Copy()
 	assert.Equal(t, set2.Size(), 3)
@@ -59,9 +59,9 @@ func TestHashSetCopy(t *testing.T) {
 }
 
 func TestHashSetUnion(t *testing.T) {
-	set1 := New()
+	set1 := New[string]()
 	set1.Add("a", "b")
-	set2 := New()
+	set2 := New[string]()
 	set2.Add("c", "d")
 	set3 := set1.Union(set2)
 	assert.Equal(t, set3.Size(), 4)
@@ -69,9 +69,9 @@ func TestHashSetUnion(t *testing.T) {
 }
 
 func TestHashSetIntersection(t *testing.T) {
-	set1 := New()
+	set1 := New[string]()
 	set1.Add("a", "b", "c", "d", "e")
-	set2 := New()
+	set2 := New[string]()
 	set2.Add("c", "d", "e", "f", "g")
 	set3 := set1.Intersection(set2)
 	assert.Equal(t, set3.Size(), 3)
@@ -79,9 +79,9 @@ func TestHashSetIntersection(t *testing.T) {
 }
 
 func TestHashSetSymmetricDifference(t *testing.T) {
-	set1 := New()
+	set1 := New[string]()
 	set1.Add("a", "b", "c", "d", "e")
-	set2 := New()
+	set2 := New[string]()
 	set2.Add("c", "d", "e", "f", "g")
 	set3 := set1.SymmetricDifference(set2)
 	assert.Equal(t, set3.Size(), 4)
@@ -89,9 +89,9 @@ func TestHashSetSymmetricDifference(t *testing.T) {
 }
 
 func TestHashSetSubtraction(t *testing.T) {
-	set1 := New()
+	set1 := New[string]()
 	set1.Add("a", "b", "c", "d", "e")
-	set2 := New()
+	set2 := New[string]()
 	set2.Add("c", "d", "e", "f", "g")
 	set3 := set1.Subtraction(set2)
 	assert.Equal(t, set3.Size(), 2)
