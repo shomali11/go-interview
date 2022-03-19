add1dbc8d673a89f29d44ed1a6a185d93cc681fd
Moein Nemati <moeinroid@gmail.com>
2022-03-19 13:56:50 +0330

    Rewrite hash multi map with generics

--- datastructures/maps/hashmultimaps/hash_multi_map.go
+++ datastructures/maps/hashmultimaps/hash_multi_map.go
@@ -1,18 +1,18 @@
 package hashmultimaps
 
 // New factory that creates a new Hash Multi Map
-func New() *HashMultiMap {
-	multiMap := HashMultiMap{data: make(map[interface{}][]interface{})}
+func New[T, V comparable]() *HashMultiMap[T, V] {
+	multiMap := HashMultiMap[T, V]{data: make(map[T][]V)}
 	return &multiMap
 }
 
 // HashMultiMap a data structure representing a map of keys with lists of values
-type HashMultiMap struct {
-	data map[interface{}][]interface{}
+type HashMultiMap[T, V comparable] struct {
+	data map[T][]V
 }
 
 // Merge merge multiple multi maps
-func (s *HashMultiMap) Merge(maps ...*HashMultiMap) {
+func (s *HashMultiMap[T, V]) Merge(maps ...*HashMultiMap[T, V]) {
 	for _, multiMap := range maps {
 		for _, key := range multiMap.GetKeys() {
 			values := multiMap.GetValues(key)
@@ -22,25 +22,25 @@ func (s *HashMultiMap) Merge(maps ...*HashMultiMap) {
 }
 
 // Put key/value pair to the multi map
-func (s *HashMultiMap) Put(key interface{}, value interface{}) {
+func (s *HashMultiMap[T, V]) Put(key T, value V) {
 	values, ok := s.data[key]
 	if !ok {
-		values = make([]interface{}, 0)
+		values = make([]V, 0)
 	}
 	values = append(values, value)
 	s.data[key] = values
 }
 
 // PutAll put key/values to the multi map
-func (s *HashMultiMap) PutAll(key interface{}, values ...interface{}) {
+func (s *HashMultiMap[T, V]) PutAll(key T, values ...V) {
 	for _, value := range values {
 		s.Put(key, value)
 	}
 }
 
 // GetKeys returns a list of the multi map's keys
-func (s *HashMultiMap) GetKeys() []interface{} {
-	keys := make([]interface{}, 0, s.Size())
+func (s *HashMultiMap[T, V]) GetKeys() []T {
+	keys := make([]T, 0, s.Size())
 	for key := range s.data {
 		keys = append(keys, key)
 	}
@@ -48,13 +48,13 @@ func (s *HashMultiMap) GetKeys() []interface{} {
 }
 
 // Contains checks if a key is in the multi map
-func (s *HashMultiMap) Contains(key interface{}) bool {
+func (s *HashMultiMap[T, V]) Contains(key T) bool {
 	_, exists := s.data[key]
 	return exists
 }
 
 // ContainsAll checks if all keys are in the multi map
-func (s *HashMultiMap) ContainsAll(keys ...interface{}) bool {
+func (s *HashMultiMap[T, V]) ContainsAll(keys ...T) bool {
 	for _, key := range keys {
 		if !s.Contains(key) {
 			return false
@@ -64,7 +64,7 @@ func (s *HashMultiMap) ContainsAll(keys ...interface{}) bool {
 }
 
 // ContainsAny checks if any keys are in the multi map
-func (s *HashMultiMap) ContainsAny(keys ...interface{}) bool {
+func (s *HashMultiMap[T, V]) ContainsAny(keys ...T) bool {
 	for _, key := range keys {
 		if s.Contains(key) {
 			return true
@@ -74,23 +74,23 @@ func (s *HashMultiMap) ContainsAny(keys ...interface{}) bool {
 }
 
 // GetValues returns values associated with the key
-func (s *HashMultiMap) GetValues(key interface{}) []interface{} {
+func (s *HashMultiMap[T, V]) GetValues(key T) []V {
 	values, ok := s.data[key]
 	if !ok {
-		return make([]interface{}, 0)
+		return make([]V, 0)
 	}
 	return values
 }
 
 // RemoveKey removes a key and all its values
-func (s *HashMultiMap) RemoveKey(keys ...interface{}) {
+func (s *HashMultiMap[T, V]) RemoveKey(keys ...T) {
 	for _, key := range keys {
 		delete(s.data, key)
 	}
 }
 
 // Remove removes a value from a key's values
-func (s *HashMultiMap) Remove(key interface{}, value interface{}) {
+func (s *HashMultiMap[T, V]) Remove(key T, value V) {
 	values, ok := s.data[key]
 	if !ok {
 		return
@@ -101,27 +101,27 @@ func (s *HashMultiMap) Remove(key interface{}, value interface{}) {
 		return
 	}
 
-	newValues := make([]interface{}, 0)
+	newValues := make([]V, 0)
 	newValues = append(newValues, values[:index]...)
 	s.data[key] = append(newValues, values[index+1:]...)
 }
 
 // Clear clears the multiMap
-func (s *HashMultiMap) Clear() {
-	s.data = make(map[interface{}][]interface{})
+func (s *HashMultiMap[T, V]) Clear() {
+	s.data = make(map[T][]V)
 }
 
 // IsEmpty checks if the multiMap is empty
-func (s *HashMultiMap) IsEmpty() bool {
+func (s *HashMultiMap[T, V]) IsEmpty() bool {
 	return s.Size() == 0
 }
 
 // Size returns size of the multiMap
-func (s *HashMultiMap) Size() int {
+func (s *HashMultiMap[T, V]) Size() int {
 	return len(s.data)
 }
 
-func getIndex(value interface{}, values ...interface{}) int {
+func getIndex[V comparable](value V, values ...V) int {
 	for i, v := range values {
 		if v == value {
 			return i
